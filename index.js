0c14b8377133a03c3cbf5e3b5c8486ebd25499a5
Expedito Andrade <expeditojazz@gmail.com>
2022-08-17 09:18:42 -0300

    fix headers

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 const getData = async () => {
   const p = document.getElementById('result')
   try {
-    const res = await axios.get('https://app.starsona.com/api/v2/partner/partner_config/')
+    const res = await axios.get('https://app.starsona.com/api/v2/partner/partner_config/', {
+      headers: {
+        device: 'web',
+        'Content-Type': 'application/json',
+      }
+    })
     if (res && res.data) {
       p.innerText = "Api call loaded"
     }
